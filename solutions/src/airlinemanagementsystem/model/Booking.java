205b07d229217ea00d6c2b2369693815d39a6af7
Nikhil Sandilya <nikhil.2.sandilya@bt.com>
2024-12-26 20:21:08 +0530

    Booking class

--- solutions/src/airlinemanagementsystem/model/Booking.java
+++ solutions/src/airlinemanagementsystem/model/Booking.java
@@ -2,6 +2,23 @@ package airlinemanagementsystem.model;
 
 public class Booking {
    private int bookingId;
+
+   public int getBookingId() {
+      return bookingId;
+   }
+
+   public void setBookingId(int bookingId) {
+      this.bookingId = bookingId;
+   }
+
+   public Payment getPayment() {
+      return payment;
+   }
+
+   public void setPayment(Payment payment) {
+      this.payment = payment;
+   }
+
    private Payment payment;
 
 
